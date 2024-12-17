83892c5cabeaa9c45a107ab1414b00e44376a570
dastonsultonov8747 <158143784+dastonsultonov8747@users.noreply.github.com>
2024-12-18 01:54:26 +0500

    Loyihaga yangiliklar qo'shildi api

--- config/settings.py
+++ config/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECRET_KEY va DEBUG ni .env dan olamiz
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=True, cast=bool)
 
 # Allowed Hosts
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
