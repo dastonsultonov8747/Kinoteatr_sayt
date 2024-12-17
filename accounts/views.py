8f940f35b689a25bc6f81c719938b0b8314ee71f
dastonsultonov8747 <158143784+dastonsultonov8747@users.noreply.github.com>
2024-12-18 03:02:20 +0500

    Ishlashi kerak adi

--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login, logout
 from .forms import CustomUserCreationForm
 
 
-def user_login(request):
+def signup_view(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
@@ -13,9 +13,9 @@ def user_login(request):
             login(request, user)
             return redirect('index')  # 'index' - bu asosiy sahifangizning URL nomi
         else:
-            return render(request, 'login.html', {'message': 'Username yoki parol noto\'g\'ri'})
+            return render(request, 'registration/login.html', {'message': 'Username yoki parol noto\'g\'ri'})
     else:
-        return render(request, 'login.html')
+        return render(request, 'registration/login.html')
 
 
 def login_view(request):
@@ -27,9 +27,9 @@ def login_view(request):
             login(request, user)
             return redirect('index')  # Muvaffaqiyatli login qilinganidan keyin bosh sahifaga
         else:
-            return render(request, 'registration/login.html', {'error': 'Invalid credentials'})
+            return render(request, 'registration/signup.html', {'error': 'Invalid credentials'})
 
-    return render(request, 'registration/login.html')
+    return render(request, 'registration/signup.html')
 
 
 def logout_view(request):
