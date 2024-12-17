3965351847242db9f8f803aa86a495d7b4de109a
dastonsultonov8747 <158143784+dastonsultonov8747@users.noreply.github.com>
2024-12-18 02:01:18 +0500

    Ishlashi kerak adi

--- config/urls.py
+++ config/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
-from movies.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
